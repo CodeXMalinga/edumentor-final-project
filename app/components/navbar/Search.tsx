07b5fd34bc99760f9bf487e941f653c87589f4ba
Malinga de Silva <rdimalingadesilva@gmail.com>
2023-07-22 00:46:42 +0530

    init

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { BiSearch } from 'react-icons/bi';
+
+const Search = () => {
+  return (
+    <div
+        className="
+            boarder-[1px]
+            w-full
+            md:w-auto
+            py-2
+            rounded-full
+            shadow-sm
+            hover:shadow-md
+            transition
+            cursor-pointer
+        "
+    >
+        <div
+        className="
+            flex
+            flex-row
+            items-center
+            justify-between
+        ">
+            <div
+                className="
+                    text-sm
+                    font-semibold
+                    px-6
+                    text-gray-500
+                "   
+            >
+                Search...
+            </div>
+
+            <div
+                className="
+                    text-sm
+                    pl-6
+                    pr-2
+                    text-gray-600
+                    flex
+                    flex-row
+                    items-center
+                    gap-3
+                "
+            >
+                <div
+                    className="
+                        p-2
+                        bg-[#4CAF50]
+                        rounded-full
+                        text-white
+                    "
+                >
+                    <BiSearch size={18} />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
