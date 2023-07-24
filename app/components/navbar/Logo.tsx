fe771c99d75fe8a036f80d46adbddb0976dd9904
Malinga de Silva <rdimalingadesilva@gmail.com>
2023-07-24 20:17:44 +0530

    added listing grid

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -12,8 +12,8 @@ const Logo = () => {
       alt="logo"
       className="hidden md:block cursor-pointer"
       height={100}
-      width={100}
-      src="/images/logo.png"
+      width={200}
+      src="/images/logo2.png"
     />
   );
 };
